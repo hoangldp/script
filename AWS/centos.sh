1fbbe707a897dfbe72cbf854235517fa16f0fe87
Lê Hoàng <hoangldp@gmail.com>
2020-01-27 17:35:54 +0700

    Start webmin

--- AWS/centos.sh
+++ AWS/centos.sh
@@ -16,4 +16,8 @@ sudo systemctl enable docker
 sudo systemctl status docker
 
 sudo docker volume create portainer_data
-sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+sudo docker run -d -p 9000:9000 --name docker_admin -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+
+# sudo passwd root
+# sudo /etc/rc.d/init.d/webmin stop
+# sudo systemctl start webmin
